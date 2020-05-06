3f4846b5ad1a1d21aa7e6ce2be960c983064f4ec
Piotrek_Skalski <piotrskalski93@gmail.com>
2020-05-06 19:26:12 +0100

    Added learner function to utils

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from torchtext.data import BucketIterator
 import torch
+from torch.nn.utils import clip_grad_value_
+import time
 import logging
 
 
@@ -15,7 +17,7 @@ def validate(ds, loss_fn, model, bs=1, device=device):
     is_in_train = model.training
     model.eval()
     with torch.no_grad():
-        size = len(ds)
+#         size = len(ds)
         predictions = []
         gt = list(ds.label)
         loss = 0
@@ -23,7 +25,7 @@ def validate(ds, loss_fn, model, bs=1, device=device):
             output = model(batch.review)
             predictions.extend(output.argmax(dim=1).tolist())
             loss += loss_fn(output, batch.label).item()
-        avg_loss = loss/size
+        avg_loss = loss/(i+1)
 
     accuracy = np.mean(np.array(predictions) == np.array(gt))
     if is_in_train: model.train()
@@ -68,3 +70,50 @@ def lr_finder(model, dataset, optimiser, loss_fn, lr_range=[1e-6, 1e0], bs=1, av
     model.load_state_dict(initial_state_dict)
     sns.lineplot(learning_rates, losses)
     plt.xscale('log')
+
+def learner(model, loss_fn, optimiser, ds_train, ds_val=None, epochs=1, bs=4, device=device, grad_clip=None):
+    """
+        A basic training loop that logs some training statistics, like losses and accuracy.
+        
+        Args:
+            model - a model class that takes one argument to its forwards method.
+            loss_fn - torch.nn module.
+            optimiser - torch.optim module.
+            ds_train - torchtext.data.Dataset used for training.
+            ds_val - torchtext.data.Dataset used for validation, default=None.
+            epochs - int; default=1.
+            bs - int; batch_size, default=4.
+            device - torch.device;
+            grad_clip - float; maximum value for a gradient clipping method,
+                        default=none, i.e. no grad clipping.
+                        
+    """
+    start_time = time.time()
+    for epoch in range(epochs):
+        
+        total_loss = 0
+        for i, batch in enumerate(BucketIterator(ds_train, bs, sort_key=lambda x: len(x.review),
+                                                 shuffle=True, device=device), 1):
+            optimiser.zero_grad()
+            
+            output = model(batch.review)
+            loss = loss_fn(output, batch.label)
+            total_loss += loss.item()
+        
+            loss.backward()
+            if grad_clip is not None:
+                clip_grad_value_(model.parameters(), grad_clip)
+            optimiser.step()
+            
+            # Logs statistics 3 times during an epoch
+            if not i % (len(ds_train)//(bs*3)):
+                avg_loss = total_loss / (len(ds_train)//(bs*3))
+                total_loss = 0
+                if ds_val is not None:
+                    val_loss, val_accuracy = validate(ds_val, loss_fn, model, bs=bs, device=device)
+                    logging.info('Epoch : {}, batch : {}, train_loss = {:.4f}, val_loss = {:.4f}, '
+                                 'val_accuracy : {:.3f}, time = {:.0f}s'
+                                 .format(epoch + 1, i, avg_loss, val_loss, val_accuracy, time.time() - start_time))
+                else:
+                    logging.info('Epoch : {}, batch : {}, train_loss = {:.4f}, time = {:.0f}s'
+                                 .format(epoch + 1, i, avg_loss, time.time() - start_time))
