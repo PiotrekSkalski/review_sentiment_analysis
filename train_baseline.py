b23069c8a9e6d6c9dc6772f4aa3ee1ddbffe23f6
Piotrek_Skalski <piotrskalski93@gmail.com>
2020-05-06 19:27:10 +0100

    Updated baseline files

--- train_baseline.py
+++ train_baseline.py
@@ -1,4 +1,4 @@
-import utils
+from utils import validate, learner
 from get_dataset import get_dataset
 import random
 import time
@@ -19,10 +19,23 @@ logging.basicConfig(filename='log.txt',
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 dataset, emb_weights = get_dataset()
-random.seed(43)
+vocab_size = len(emb_weights)
+embed_size = emb_weights.shape[1] # 300
+dataset.fields['review'].include_lengths = True
+# the above line adds a tensor with lengths of reviews in a batch, so that we can pass batches to embeddingbag
+
+# random.seed(43)
 ds_train, ds_val, ds_test = dataset.split(split_ratio=[0.8, 0.1, 0.1], random_state=random.getstate())
 
 class Baseline_model(nn.Module):
+  """
+     Basic baseline model based on a bag of embeddings.
+     
+     Args:
+         vocab_size - int; size of the dataset vocabulary .
+         embed_dim - int; size of the embedding vectors.
+         embed_vecs - tensor; pretrained word embeddings (optional).
+  """
   def __init__(self, vocab_size, embed_dim, embed_vecs=None):
     super().__init__()
     self.embedding = nn.EmbeddingBag(vocab_size, embed_dim)
@@ -30,39 +43,20 @@ class Baseline_model(nn.Module):
       self.embedding = nn.EmbeddingBag.from_pretrained(embed_vecs)
     else:
       self.embedding = nn.EmbeddingBag(vocab_size, embed_dim)
-    self.fc1 = nn.Linear(embed_dim, embed_dim//2)
-    self.fc2 = nn.Linear(embed_dim//2, 2)
-    self.relu = nn.ReLU()
+    self.head = nn.Sequential(nn.Linear(embed_dim, embed_dim//2),
+                              nn.ReLU(),
+                              nn.Linear(embed_dim//2, 2))
 
-  def forward(self, x):
-    out = self.embedding(x)
-    out = self.relu(self.fc1(out))
-    return self.fc2(out)
-
-def learner(model, loss_fn, optimiser, epochs=1, device=device):
-  start_time = time.time()
-  for epoch in range(epochs):
+  def forward(self, batch):
+    batch, lengths = batch
     
-    total_loss = 0
-    for i, batch in enumerate(Iterator(ds_train, 1, shuffle=True, device=device), 1):
-      optimiser.zero_grad()
+    batch_concat = torch.cat([review[:length] for review, length in zip(batch, lengths)], dim=0)
+    offsets = torch.cat([torch.tensor([0]), torch.cumsum(lengths[:-1], dim=0)])
+    
+    out = self.embedding(batch_concat, offsets)
+    return self.head(out)
 
-      output = model(batch.review)
-      loss = loss_fn(output, batch.label)
-      total_loss += loss.item()
 
-      loss.backward()
-      optimiser.step()
-
-      if not i % (len(ds_train)//3):
-        avg_loss = total_loss / (len(ds_train)//3)
-        val_loss, val_accuracy = utils.validate(ds_val, loss_fn, model)
-        logging.info('Epoch : {}, batch : {}, train_loss = {:.4f}, val_loss = {:.4f}, val_accuracy : {:.3f}'
-                     .format(epoch + 1, i,avg_loss, val_loss, val_accuracy))
-        total_loss = 0
-
-vocab_size = len(emb_weights)
-embed_size = 300
 
 logging.info('Initialising the model with the embedding layer frozen.')
 model = Baseline_model(vocab_size, embed_size, emb_weights).to(device)
@@ -70,7 +64,7 @@ loss_fn = nn.CrossEntropyLoss().to(device)
 optimiser = Adam(model.parameters(), lr=2e-4)
 
 logging.info('Training, lr=2e-4')
-learner(model, loss_fn, optimiser, epochs=5)
+learner(model, loss_fn, optimiser, ds_train, ds_val, epochs=10, bs=8)
 if not os.path.exists('models'):
     os.makedirs('models')
     
@@ -82,10 +76,10 @@ model.embedding.weight.requires_grad_(True);
 optimiser = Adam(model.parameters(), lr=1e-4)
 
 logging.info('Fine-tuning, lr=1e-4')
-learner(model, loss_fn, optimiser, epochs=5)
+learner(model, loss_fn, optimiser, ds_train, ds_val, epochs=10, bs=8)
 
 logging.info('Model saved to: models/model_baseline_fine.pt')
 torch.save(model.state_dict(), 'models/model_baseline_fine.pt')
 
 logging.info('--- Evaluating on the test set ---')
-logging.info('Test loss : {:.5f}, test accuracy : {:.03f}'.format(*utils.validate(ds_test, loss_fn, model)))
+logging.info('Test loss : {:.5f}, test accuracy : {:.03f}'.format(*validate(ds_test, loss_fn, model, bs=8)))
